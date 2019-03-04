0e34d801e3683d46eb8c83730517f4d6c3c32af2
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2019-03-05 07:25:57 +0900

    Update to deploy rinkeby

---
+++ src/tools/wakeup.js
@@ -0,0 +1,24 @@
+const Web3 = require('web3')
+const web3 = new Web3();
+web3.setProvider(new Web3.providers.HttpProvider("https://rinkeby.infura.io/"));
+const privateKey = require('../../truffle-config').privatekey
+const ownerAccount = web3.eth.accounts.privateKeyToAccount('0x' + privateKey)
+web3.eth.accounts.wallet.add(ownerAccount); // web3.eth.accounts.wallet[0] == ownerAccount
+
+let tenDaysWakeUpDevil_abi = require('../../build/contracts/TenDaysWakeUpDevil.json').abi
+let tenDaysWakeUpDevil_address = require('../../build/contracts/TenDaysWakeUpDevil.json').networks['4'].address
+const TenDaysWakeUpDevil = new web3.eth.Contract(tenDaysWakeUpDevil_abi, tenDaysWakeUpDevil_address)
+for(let contract of [TenDaysWakeUpDevil]){
+    contract.options.from = web3.eth.accounts.wallet[0].address; // default from address
+    contract.options.gasPrice = '1000000000'; // default gas price in wei (1 Gwei)
+    contract.options.gas = 3000000; // provide as fallback always 3M gas
+}
+
+const main = async () => {
+    console.log('start wakeup')
+    let tx = await TenDaysWakeUpDevil.methods.wakeUp(1).send()
+    console.log('finish wakeup')
+    console.log(tx)
+}
+
+main()
--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,23 @@
 'use strict';
 
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = [`<replace with deployer's private key>`]
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 7545,
       network_id: '*'
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(privateKeys, 'https://rinkeby.infura.io/');
+      },
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 1000000000 // 1 [GWei]
     }
-  }
+  },
+  privatekey: privateKeys[0]
 };
